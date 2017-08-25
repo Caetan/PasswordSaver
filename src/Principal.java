1953346bd3369661bb75f3e16b220e002542bdb5
Caetan <caetantojeiro95@gmail.com>
2017-08-25 20:39:14 +0200

    Primera version

---
+++ src/Principal.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.io.*; 
+import java.util.Scanner; 
+import javax.swing.JOptionPane;
+
+/* 
+ * Aplicación para guardar contraseñas 
+ * 
+ * @author Caetán Tojeiro Carpente     [/color][/color][/color][/color]
+[color=#000000][color=#000000][color=#000000][color=#000000] *  caetantojeiro95@gmail.com 
+LinkedIn -> https://www.linkedin.com/in/caetan/
+ */ 
+
+import interfaces.Ventana;
+
+
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		new Ventana().setVisible(true);
+	}
+}
---
+++ src/interfaces/PanelInicial.java
@@ -0,0 +1,173 @@
+package interfaces;
+
+import javax.swing.JPanel;
+
+
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.Desktop;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.awt.event.ActionEvent;
+
+public class PanelInicial extends JPanel {
+	private JTextField tfNombre;
+	private final JLabel lblNewLabel = new JLabel("Descripcion");
+	private JTextField tfDescripcion;
+	private final JLabel lblContrasea = new JLabel("Contrasena");
+	private JTextField tfContrasena;
+	private JLabel lblNewLabel_1;
+	private JButton btnVerContrasenas;
+
+	/**
+	 * Create the panel.
+	 */
+	
+	
+	File fichero = new File(javax.swing.filechooser.FileSystemView.getFileSystemView().getHomeDirectory() + "\\BANCO_DE_CONTRASENAS.doc");//Archivo usado para guardar y leer los datos, con ruta
+	//relativa a nuestro .jar
+	String Nombre, Descripcion, Contrasena;
+
+
+
+	
+	public PanelInicial() {
+		setLayout(new GridLayout(9, 2, 5, 5));
+
+
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 17));
+		add(lblNombre);
+		
+		tfNombre = new JTextField();
+		add(tfNombre);
+		tfNombre.setColumns(10);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+		add(lblNewLabel);
+		
+		tfDescripcion = new JTextField();
+		add(tfDescripcion);
+		tfDescripcion.setColumns(10);
+		lblContrasea.setFont(new Font("Tahoma", Font.BOLD, 17));
+		add(lblContrasea);
+		
+		tfContrasena = new JTextField();
+		add(tfContrasena);
+		tfContrasena.setColumns(10);
+		
+		JButton btnGuardar = new JButton("GUARDAR");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Guardar datos
+				Nombre = tfNombre.getText();
+				Descripcion = tfDescripcion.getText();
+				Contrasena = tfContrasena.getText();
+				mostrarDatos();
+				guardarDatos(Nombre,Descripcion,Contrasena);
+				
+			}
+
+		});
+		add(btnGuardar);
+		
+		lblNewLabel_1 = new JLabel("");
+		add(lblNewLabel_1);
+		
+		btnVerContrasenas = new JButton("VER CONTRASENAS");
+		btnVerContrasenas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Ver contraseñas
+				leerDatos();
+			//	mostrarDatos();
+				abrirFichero();
+			}
+            
+
+		});
+		add(btnVerContrasenas);
+
+	}
+	
+	public String darNombre() {
+		return tfNombre.getText();
+	}
+	
+	public String darDescripcion() {
+		return tfDescripcion.getText();
+	}
+	
+	public String darContrasena() {
+		return tfContrasena.getText();
+	}
+	
+	public void limpiarCampos() {
+		tfNombre.setText("");
+		tfDescripcion.setText("");
+		tfContrasena.setText("");
+	}
+	
+    
+	private void mostrarDatos() {
+		System.out.println(Nombre + "\n");
+		System.out.println(Descripcion + "\n");
+		System.out.println(Contrasena + "\n");
+		System.out.println("------------------------------------------------------------------------------------------------" + "\n");
+	}
+
+	private void leerDatos() {
+        String linea = null;//Para guardar los datos de forma temporal 
+        if (fichero.exists()) {//Si se ha creado el fichero 
+            try { 
+                Scanner lector = new Scanner(fichero); 
+                while(lector.hasNextLine()){ 
+                linea=lector.nextLine(); 
+                } 
+                 String[] datos = linea.split("-,-"); 
+                 Nombre = datos[0]; 
+                 Descripcion = datos[1]; 
+                 Contrasena = datos[2]; 
+
+            } catch (FileNotFoundException ex) { 
+                System.out.print("No se pudo abrir el archivo!"+ex.getMessage()); 
+            } 
+        } 				
+	}
+	
+
+	private void guardarDatos(String nombre, String descripcion, String contrasena) {
+        try { 
+            FileWriter bw;//Instancia de BW 
+            bw = new FileWriter(fichero,true);//Inicialización de "BW" con "FW" como parámetro con "fichero" como parámetro
+            bw.write("-- 1. NOMBRE: " + nombre + "-,-" + "2. DESCRIPCIÓN: "+ descripcion + "-,-" + "3. CONTRASEÑA: " + contrasena + "\r\n");//Escribimos lo que haya en los TextFields
+            //y entre ello agregamos una coma ("-,-"). 
+            bw.close();//Cerramos para guardar los datos en el fichero 
+        } catch (IOException ex) { 
+            System.out.print("No se pudieron guardar los datos!n" + ex.getMessage()); 
+        } 
+	}
+	
+	
+	public void abrirFichero(){
+
+	     try {
+	            Desktop.getDesktop().open(fichero);
+
+	     }catch (IOException ex) {
+
+	            System.out.println(ex);
+
+	     }
+
+	}        
+
+}
